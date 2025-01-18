d1490033fd61012354883948d88a884a967e5724
Camilo Gallego <gckcamilo@gmail.com>
2025-01-18 11:26:09 -0500

    configuracion de redux, creacion de store, vincular provider, creacion de custom hooks, prueba basica de integracion

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import {SafeAreaView,ScrollView,StatusBar,StyleSheet,Text,useColorScheme,View} from 'react-native';
+import {SafeAreaView,ScrollView,StatusBar,StyleSheet,Text,TouchableOpacity,useColorScheme,View} from 'react-native';
 import { Colors, Header} from 'react-native/Libraries/NewAppScreen';
+import { useAppDispatch, useAppSelector } from './src/hooks/reduxHook';
+import { increment } from './src/slices/counter/counterSlice';
 
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const dispatch = useAppDispatch()
+  const handleIncrement = () => dispatch(increment())
+
+
+  const counter = useAppSelector((state) => state.counter.value);
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -15,10 +22,13 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView  contentInsetAdjustmentBehavior="automatic"  style={backgroundStyle}>
+      <ScrollView  contentInsetAdjustmentBehavior="automatic"  style={backgroundStyle} contentContainerStyle={{justifyContent:'center', alignItems:'center'}}  >
         <View style={{  backgroundColor: isDarkMode ? Colors.black : Colors.white }}>
-          <Text style={{color: isDarkMode ? Colors.white : Colors.black}}>Configuración folders</Text>
+          <Text style={{color: isDarkMode ? Colors.white : Colors.black}}>Configuración folders {counter} </Text>
         </View>
+        <TouchableOpacity onPress={handleIncrement} style={{marginTop: 20}}>
+          <Text style={{color: isDarkMode ? Colors.white : Colors.black}}>incrementar </Text>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
--- index.js
+++ index.js
@@ -5,8 +5,18 @@
 import {AppRegistry, Text, TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { Provider } from 'react-redux';
+import { store } from './src/store';
 
 Text.defaultProps = { ...(Text.defaultProps || {}), allowFontScaling: false };
 TextInput.defaultProps = { ...(TextInput.defaultProps || {}), allowFontScaling: false };
 
-AppRegistry.registerComponent(appName, () => App);
+const RootApp = () =>{
+    return(
+        <Provider store={store} >
+            <App/>
+        </Provider>
+    );
+};
+
+AppRegistry.registerComponent(appName, () => RootApp);
